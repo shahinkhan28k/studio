7496a3e84e10e88a1e70502e4bf41f70eca65f65
Shahin Khan <shahinkhan28r@gmail.com>
2025-08-05 03:53:26 +0000

    Console Error
    FirebaseError: Missing or insufficient permissions প্রবল

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,8 +6,9 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCh3OwU5XxzJfKuGrhcf0xf_7Tg5D3ja3g",
   authDomain: "onearn-platform.firebaseapp.com",
+  databaseURL: "https://onearn-platform-default-rtdb.firebaseio.com",
   projectId: "onearn-platform",
-  storageBucket: "onearn-platform.appspot.com",
+  storageBucket: "onearn-platform.firebasestorage.app",
   messagingSenderId: "350477123762",
   appId: "1:350477123762:web:09b7987cf92882d04d09ab"
 };
