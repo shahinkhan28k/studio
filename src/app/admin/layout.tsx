e95a8f846c9dc18bc3b2383c3a0a6e258d61d4c2
Shahin Khan <shahinkhan28r@gmail.com>
2025-08-06 04:14:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -19,6 +19,7 @@ import { Home, Users, ClipboardList, Bell, Settings, Percent, LayoutDashboard, U
 import { useAuth } from "@/context/auth-context"
 import { useRouter } from "next/navigation"
 import React from "react"
+import { AppProvider } from "@/context/app-context"
 
 
 export default function AdminLayout({
@@ -40,96 +41,98 @@ export default function AdminLayout({
     }
 
   return (
-    <SidebarProvider>
-      <Sidebar side="left" collapsible="icon">
-        <SidebarHeader>
-           <SidebarTrigger />
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton href="/admin" tooltip="Dashboard">
-                <Home />
-                <span>Dashboard</span>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-             <SidebarGroup>
-                <SidebarGroupLabel>Content</SidebarGroupLabel>
-                 <SidebarMenuItem>
-                  <SidebarMenuButton href="/admin/notices" tooltip="Notices">
-                    <Bell />
-                    <span>Notices</span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-             </SidebarGroup>
-            
-             <SidebarSeparator />
-
-             <SidebarGroup>
-                <SidebarGroupLabel>Users</SidebarGroupLabel>
-                 <SidebarMenuItem>
-                  <SidebarMenuButton href="/admin/users" tooltip="Users">
-                    <Users />
-                    <span>Users</span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-             </SidebarGroup>
-
-            <SidebarSeparator />
-
-            <SidebarGroup>
-                <SidebarGroupLabel>Earnings</SidebarGroupLabel>
+    <AppProvider>
+        <SidebarProvider>
+        <Sidebar side="left" collapsible="icon">
+            <SidebarHeader>
+            <SidebarTrigger />
+            </SidebarHeader>
+            <SidebarContent>
+            <SidebarMenu>
                 <SidebarMenuItem>
-                  <SidebarMenuButton href="/admin/tasks" tooltip="Tasks">
-                    <ClipboardList />
-                    <span>Tasks</span>
-                  </SidebarMenuButton>
+                <SidebarMenuButton href="/admin" tooltip="Dashboard">
+                    <Home />
+                    <span>Dashboard</span>
+                </SidebarMenuButton>
                 </SidebarMenuItem>
-                 <SidebarMenuItem>
-                  <SidebarMenuButton href="/admin/referrals" tooltip="Referrals">
-                    <Percent />
-                    <span>Referrals</span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-            </SidebarGroup>
-            
-            <SidebarSeparator />
+                <SidebarGroup>
+                    <SidebarGroupLabel>Content</SidebarGroupLabel>
+                    <SidebarMenuItem>
+                    <SidebarMenuButton href="/admin/notices" tooltip="Notices">
+                        <Bell />
+                        <span>Notices</span>
+                    </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarGroup>
+                
+                <SidebarSeparator />
 
-             <SidebarGroup>
-                <SidebarGroupLabel>Transactions</SidebarGroupLabel>
-                 <SidebarMenuItem>
-                  <SidebarMenuButton href="/admin/transactions/deposits" tooltip="Deposits">
-                    <ArrowDownToLine />
-                    <span>Deposits</span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-                  <SidebarMenuItem>
-                  <SidebarMenuButton href="/admin/transactions/withdrawals" tooltip="Withdrawals">
-                    <ArrowUpFromLine />
-                    <span>Withdrawals</span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-            </SidebarGroup>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Users</SidebarGroupLabel>
+                    <SidebarMenuItem>
+                    <SidebarMenuButton href="/admin/users" tooltip="Users">
+                        <Users />
+                        <span>Users</span>
+                    </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarGroup>
+
+                <SidebarSeparator />
+
+                <SidebarGroup>
+                    <SidebarGroupLabel>Earnings</SidebarGroupLabel>
+                    <SidebarMenuItem>
+                    <SidebarMenuButton href="/admin/tasks" tooltip="Tasks">
+                        <ClipboardList />
+                        <span>Tasks</span>
+                    </SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                    <SidebarMenuButton href="/admin/referrals" tooltip="Referrals">
+                        <Percent />
+                        <span>Referrals</span>
+                    </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarGroup>
+                
+                <SidebarSeparator />
+
+                <SidebarGroup>
+                    <SidebarGroupLabel>Transactions</SidebarGroupLabel>
+                    <SidebarMenuItem>
+                    <SidebarMenuButton href="/admin/transactions/deposits" tooltip="Deposits">
+                        <ArrowDownToLine />
+                        <span>Deposits</span>
+                    </SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                    <SidebarMenuButton href="/admin/transactions/withdrawals" tooltip="Withdrawals">
+                        <ArrowUpFromLine />
+                        <span>Withdrawals</span>
+                    </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarGroup>
 
 
-            <SidebarSeparator />
+                <SidebarSeparator />
 
-             <SidebarGroup>
-                <SidebarGroupLabel>Platform</SidebarGroupLabel>
-                 <SidebarMenuItem>
-                  <SidebarMenuButton href="/admin/settings" tooltip="Settings">
-                    <Settings />
-                    <span>Settings</span>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-            </SidebarGroup>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Platform</SidebarGroupLabel>
+                    <SidebarMenuItem>
+                    <SidebarMenuButton href="/admin/settings" tooltip="Settings">
+                        <Settings />
+                        <span>Settings</span>
+                    </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarGroup>
 
-          </SidebarMenu>
-        </SidebarContent>
-      </Sidebar>
-      <SidebarInset>
-        <main>{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+            </SidebarMenu>
+            </SidebarContent>
+        </Sidebar>
+        <SidebarInset>
+            <main>{children}</main>
+        </SidebarInset>
+        </SidebarProvider>
+    </AppProvider>
   )
 }
